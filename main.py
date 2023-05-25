6568e9db8c214d147336d1b6e7daaf8e39f55c04
Ridful <19477176+Ridful@users.noreply.github.com>
2023-05-25 10:54:04 +0200

    grabs most relevant data on paste page

--- main.py
+++ main.py
@@ -7,14 +7,15 @@ from lxml import html
 active_url = urlparse("https://www.pastebin.com/")
 
 parser = argparse.ArgumentParser(description='description of program here')
-parser.add_argument('arg_name', help='Help message for the do argument')
+parser.add_argument('scan', help='Help message for the do argument')
 parser.add_argument('-p', '--pasteid', help='specify the unique paste path id')
-parser.add_argument('-t', '--tryconnect', action='store_true', help='try the connection')
+parser.add_argument('-e', '--extractinfo', action='store_true', help='try the connection')
 args = parser.parse_args()
 
-arg_value = args.arg_name
+arg_value = args.scan
 arg_pasteid = args.pasteid
-arg_tryconnect = args.tryconnect
+arg_extractinfo = args.extractinfo
+#get_raw_paste = args.getrawpaste
 
 def getUrl(pasteUniquePathId):
     temp_url_tuple = active_url._replace(path=pasteUniquePathId)
@@ -25,29 +26,51 @@ def getPastePageBytes(full_url):
 
 def pageParser(pastePage):
     tree = html.fromstring(pastePage.content)
-    xpath = "/html/body/div[1]/div[2]/div[1]/div[2]/div[2]/div[3]/div[1]/h1/text()"
-    element = tree.xpath(xpath)
-    if element:
-        text = element[0].text
-        print(f'title of paste: {text}')
     
+    xpath_dict = {
+        'title_xpath': "/html/body/div[1]/div[2]/div[1]/div[2]/div[2]/div[3]/div[1]/h1/text()",
+        'userUploader_xpath': "/html/body/div[1]/div[2]/div[1]/div[2]/div[2]/div[3]/div[2]/div[1]/a/text()",
+        'uploadDate_xpath': "/html/body/div[1]/div[2]/div[1]/div[2]/div[2]/div[3]/div[2]/div[2]/span/text()",
+        'visits_xpath': "/html/body/div[1]/div[2]/div[1]/div[2]/div[2]/div[3]/div[2]/div[3]/text()",
+        'stars_xpath': "/html/body/div[1]/div[2]/div[1]/div[2]/div[2]/div[3]/div[2]/div[4]/text()",
+        'expiry_xpath': "/html/body/div[1]/div[2]/div[1]/div[2]/div[2]/div[3]/div[2]/div[5]/text()",
+        'syntax_xpath': "/html/body/div[1]/div[2]/div[1]/div[2]/div[4]/div[1]/div[1]/a[1]/text()",
+        'filesize_xpath': "/html/body/div[1]/div[2]/div[1]/div[2]/div[4]/div[1]/div[1]/text()[1]/text()",
+        'category_xpath': "/html/body/div[1]/div[2]/div[1]/div[2]/div[4]/div[1]/div[1]/span/text()",
+        'thumbsup_xpath': "/html/body/div[1]/div[2]/div[1]/div[2]/div[4]/div[1]/div[1]/a[2]/text()",
+        'thumbsdown_xpath': "/html/body/div[1]/div[2]/div[1]/div[2]/div[4]/div[1]/div[1]/a[3]/text()",
+        'extra_xpath': "/html/body/script[4]/text()"
+    }
+    
+    result_dict = {}
+    for variable, xpath in xpath_dict.items():
+        elements = tree.xpath(xpath)
+        if elements:
+            element = elements[0]
+            print(f'Found: {element.strip()}')
+            result_dict[variable] = str(element).strip()
+                
+    #print('\t'.join(result_dict.values()))
+    
+    return result_dict
+
 def main():
     
     if arg_value and arg_pasteid:
-        print(arg_pasteid)
+
+        paste_url = getUrl(arg_pasteid)
         
-        my_url = getUrl(arg_pasteid)
-        
-        if arg_tryconnect:
-            res = getPastePageBytes(my_url)
+        if arg_extractinfo:
+            res = getPastePageBytes(paste_url)
             
-            print(f'my_input: {arg_pasteid}  |  my_url: {my_url}  |  page_status_code: {res.status_code}\n')
+            print(f'my_input: {arg_pasteid}  |  paste_url: {paste_url}  |  page_status_code: {res.status_code}\n')
 
             if res.status_code == 200:
-                pageParser(res)
-            #print(res.text)
+                result_dict = pageParser(res)
+                for key,value in result_dict.items():
+                    print(f'{key}: {value}')
         else:
-            print(f'my_input: {arg_pasteid}  |  my_url: {my_url}  |  page_status_code: N/A')
+            print(f'my_input: {arg_pasteid}  |  paste_url: {paste_url}  |  page_status_code: N/A')
             
 if __name__ == "__main__":
     main()
