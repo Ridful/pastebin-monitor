bf33500a577586e72b8cae4d0fe62cbdd52e1418
Ridful <19477176+Ridful@users.noreply.github.com>
2023-06-03 16:37:03 +0200

    fix

--- .venv/bin/activate.fish
+++ .venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/mu/Development/github/PastebinScraper/.venv"
+set -gx VIRTUAL_ENV "/home/mu/Development/github/pastebin-monitor/.venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
